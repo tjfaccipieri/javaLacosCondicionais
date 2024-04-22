3a39b5817779a8d58b5af0205051489a695dbc33
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-04-22 10:57:32 -0300

    comentarios

--- src/lacoscondicionais/Calculadora.java
+++ src/lacoscondicionais/Calculadora.java
@@ -6,14 +6,17 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// importar o método Scanner, para receber entrada de dados do usuário
 		Scanner leia = new Scanner(System.in);
 		
+		// declaração das variaveis que iremos usar no exemplo
 		int numero1, numero2, opcao, resultado;
 		
+		//solicitar a primeira entrada de dados do usuario
 		System.out.println("Por favor, ó grande usuário, digite o primeiro numero:");
 		numero1 = leia.nextInt();
 		
+		//solicitar a segunda entrada de dados do usuario
 		System.out.println("Por favor, ó grande usuário, digite o segundo numero:");
 		numero2 = leia.nextInt();
 		
@@ -23,12 +26,16 @@ public class Calculadora {
         System.out.println("*** 3 - Multiplicar os numeros ***");
         System.out.println("*** 4 - Dividir o primeiro pelo segundo ***");
 
+        //solicitar a escolha da opção
         opcao = leia.nextInt();
         
+        
+        // verificar qual opção foi selecionada, e direcionar para o caso especifico
         switch(opcao) {
         	case 1:
         		resultado = numero1 + numero2;
         		System.out.println("O resultado da soma é: " + resultado);
+        		// sempre dar um BREAK, para finalizar a execução do código
         		break;
     		
         	case 2:
@@ -51,6 +58,8 @@ public class Calculadora {
         		
         	case 4:
         }
+        // ao final do programa, fechar a execução do Scanner, com a linha abaixo
+        leia.close();
 
 	}
 
